d1237239dbd99fdfb2758f028bd480ef2fd99def
Sasha Fedosimova <sasha.fedosimova@mail.ru>
2021-06-08 22:47:52 +0300

    make it better

---
+++ roberto.py
@@ -0,0 +1,288 @@
+kill1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам и идти по делам:\n\n➤Контракт: Убийство\nФаза активации: любая\nДействие контракта: ты заключишь контракт с синьором Бесом, и единоразово он уберёт с твоей дороги одного твоего врага.\nДополнительно: активируется в текущую фазу на любого игрока."
+heal1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Исцеление\nФаза активации: любая\nДействие контракта: ты заключишь контракт с донной Тарой, и единоразово в трансплантологической клинике тебе, или игроку по твоему выбору, осуществят пересадку из подпольного банка органов.\nДополнительно: можно использовать как на себя, так и на другого игрока. Можно активировать только при угрозе жизни. Активируется в текущую фазу, можно оставить распоряжение на активацию в следующую с указанием точных условий активации."
+find1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Раскрытие роли\nФаза активации: любая\nДействие контракта: ты заключишь контракт с доном Алом, и единоразово он раскроет тебе роль человека, на которого ты укажешь.\nДополнительно: при сообщении результата держателю контракта, действия роли Миледи не учитываются. Активируется в текущую фазу, результат приходит после окончания фазы, но до итогов (день – 16:00, ночь – 22:00)."
+close1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Подмена роли\nФаза активации: любая\nДействие контракта: ты заключишь контракт к синьором Рабастаном, и единоразово он подменит ваше досье в случае проверки.\nДополнительно: можно использовать как на себя, так и на другого игрока. При активации контракта вы заблаговременно указываете желаемую роль. Именно она будет сообщаться игроку, решившему вас проверить. В случае проверки Шерифа или Дона результатом проверки будет сторона выбранной вами роли"
+immune1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Иммунитет\nФаза активации: любая\nДействие контракта: ты заключишь контракт с синьориной Кэнди, и единоразово синьорина Кэнди подарит тебе экскурсию по своему заведению. Это время ты проведёшь безопасно… и сладко.\nДополнительно: можно использовать как на себя, так и на другого игрока. Активируется в текущую фазу. При активации контракта вы, или другой игрок, на которого вы активируете контракт, будете под защитой от любого воздействия – лечение, убийство, проверка, и так далее, но если у вас активная ночная или дневная роль и фаза активации совпала с ходом вашей роли, вы также не сможете совершать каких-либо действий. При использовании в дневную фазу контракт лишает вас права голоса на дневном голосовании."
+whisky1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Бутылка виски\nФаза активации: ночная\nКоличество контрактов в рандоме: 2 шт.\nДействие контракта: ты заключишь контракт с синьориной Эви, и единоразово она устроит тебе персональный алкотур по самым крутым барам Бергамо.\nДополнительно: активируется только на другого игрока в ночную фазу. На следующий день выбранный игрок не сможет голосовать, а так же общаться в игровых беседах и альянсах. Если у выбранного игрока активная дневная роль, он также не сможет совершить свой ход."
+ropes1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Бондаж\nФаза активации: любая\nКоличество контрактов в рандоме: 2 шт.\nДействие контракта: ты заключишь контракт с синьором Элиасом, и единоразово он покажет тебе на практике все прелести элитного БДСМ клуба.\nДополнительно: активируется в любую фазу, но только на другого игрока. Если фаза активации совпадает с ходом активной роли игрока, он не сможет совершить каких-либо действий. Не лишает выбранного игрока права голоса, если активирован ночью. В случае активации в дневную фазу, выбранный игрок лишается права голоса."
+detect1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Частное расследование\nФаза активации: дневная\nКоличество контрактов в рандоме: 2 шт.\nДействие контракта: ты заключишь контракт с синьориной Трики, и единоразово она позволит тебе выпустить дополнительную статью журналиста.\nДополнительно: активируется в дневную фазу (до 16.00) на любых игроков, кроме себя и тех, кто уже был упомянут в основных статьях журналиста."
+seek1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Слежка\nФаза активации: любая\nКоличество контрактов в рандоме: 2 шт.\nДействие контракта: ты заключишь контракт с синьором Джо и синьориной Черри, и единоразово они устроят профессиональную слежку за игроком, на которого ты укажешь.\nДополнительно: Сообщается только первое совершённое за фазу игроком действие, будь оно действием роли или контракта. Активируется в текущую фазу, результат приходит после окончания фазы, но до итогов (день – после 16:00, ночь – после 22:00)."
+rumour1 = "Отличный выбор, amico! Поздравляю тебя с успешным началом смены. Свой отряд знаешь? Отлично. Можешь забирать и идти по делам:\n\n➤Контракт: Афера\nФаза активации: ночная\nКоличество контрактов в рандоме: 2 шт.\nДействие контракта: ты заключишь контракт с синьором Антоном, и единоразово он распустит возмутительные сплетни о том, на кого ты укажешь.\nДополнительно: выбирается два игрока — тот, который будет аргументировать, и тот, против которого будут аргументировать в следующую дневную фазу. Жертва контракта не имеет права говорить остальным игрокам о том, что она попала под влияние контракта, а также отмалчиваться на обсуждении и быть неубедительным в своей аргументации."
+
+kill3 = ['photo-205080022_457239021']
+whisky3 = ['photo-205080022_457239020']
+find3 = ['photo-205080022_457239022']
+heal3 = ['photo-205080022_457239023']
+ropes3 = ['photo-205080022_457239024']
+detect3 = ['photo-205080022_457239025']
+close3 = ['photo-205080022_457239027']
+immune3 = ['photo-205080022_457239028']
+seek3 = ['photo-205080022_457239029']
+rumour3 = ['photo-205080022_457239030']
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api import VkUpload
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+keyboard = VkKeyboard(inline=True) #потестить с =False
+keyboard.add_button('Взять иммунитет', color=VkKeyboardColor.PRIMARY) #POSITIVE зелёный, NEGATIVE красный, PRIMARY синий
+keyboard.add_button('Взять исцеление', color=VkKeyboardColor.PRIMARY)
+keyboard.add_line()
+keyboard.add_button('Взять частное расследование')
+keyboard.add_button('Взять слежку')
+keyboard.add_line()
+keyboard.add_button('Взять раскрытие роли', color=VkKeyboardColor.PRIMARY)
+keyboard.add_button('Взять подмену роли', color=VkKeyboardColor.PRIMARY)
+keyboard.add_line()
+keyboard.add_button('Взять убийство')
+keyboard.add_button('Взять бутылку виски')
+keyboard.add_line()
+keyboard.add_button('Взять бондаж', color=VkKeyboardColor.PRIMARY)
+keyboard.add_button('Взять аферу', color=VkKeyboardColor.PRIMARY)
+
+
+def send_message(sender, message):
+    authorize.method('messages.send', {'user_id': sender, 'message': message, 'random_id': get_random_id()})
+
+def send_button(sender, message):
+    authorize.method('messages.send', {'user_id': sender, 'message': message, 'random_id': get_random_id(), 'keyboard': keyboard.get_keyboard()})
+    
+def get_name(sayer_id):
+    sender_info = getting_api.users.get(user_ids = sayer_id)[0]
+    full_name = sender_info.get('first_name') + ' ' + sender_info['last_name']
+    return full_name
+
+def deal(sender, attachment, message):
+    authorize.method('messages.send', {'user_id': sender, 'message': message, 'attachment': ','.join(attachment), 'random_id': get_random_id()})
+
+def finally_end():
+    itog = "Распределение контрактов:"
+    itog += "\n\nИММУНИТЕТ:\n"
+    for j in range(len(immune)):
+        itog += immune[j] + "\n"
+        j += 1
+    itog += "\n\nУБИЙСТВО:\n"
+    for j in range(len(kill)):
+        itog += kill[j] + "\n"
+        j += 1
+    itog += "\n\nИСЦЕЛЕНИЕ:\n"
+    for j in range(len(heal)):
+        itog += heal[j] + "\n"
+        j += 1
+    itog += "\n\nРАСКРЫТИЕ РОЛИ:\n"
+    for j in range(len(find)):
+        itog += find[j] + "\n"
+        j += 1
+    itog += "\n\nПОДМЕНА РОЛИ:\n"
+    for j in range(len(close)):
+        itog += close[j] + "\n"
+        j += 1
+    itog += "\n\nБУТЫЛКА ВИСКИ:\n"
+    for j in range(len(whisky)):
+        itog += whisky[j] + "\n"
+        j += 1
+    itog += "\n\nБОНДАЖ:\n"
+    for j in range(len(ropes)):
+        itog += ropes[j] + "\n"
+        j += 1
+    itog += "\n\nЧАСТНОЕ РАССЛЕДОВАНИЕ:\n"
+    for j in range(len(detect)):
+        itog += detect[j] + "\n"
+        j += 1
+    itog += "\n\nСЛЕЖКА:\n"
+    for j in range(len(seek)):
+        itog += seek[j] + "\n"
+        j += 1
+    itog += "\n\nАФЕРА:\n"
+    for j in range(len(rumour)):
+        itog += rumour[j] + "\n"
+        j += 1
+    return itog
+
+people = []
+
+token = "e5201b2dd4098be964a3752527840004fa38a7cb09a07696baf5358c9dff2396bebd04fe4f5df513790f8"
+authorize = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(authorize)
+getting_api = authorize.get_api()
+upload = VkUpload(authorize)
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        #admins = [605574836]
+        admins = [290628062, 263861517, 434758797, 338010077, 620899095, 448935345, 531102499, 471824402, 625633090]
+        received_message = event.text
+        rm = received_message.lower()
+        sender = event.user_id
+        sayer_name = get_name(sender)
+        
+        if rm == "аукцион":
+            i = 0
+            for i in range(len(admins)):
+                if sender == admins[i]:
+                    kill0 = 6
+                    heal0 = 6
+                    find0 = 6
+                    close0 = 6
+                    immune0 = 4
+                    whisky0 = 3
+                    ropes0 = 3
+                    detect0 = 2
+                    seek0 = 5
+                    rumour0 = 2
+                    
+                    kill = []
+                    heal = []
+                    find = []
+                    close = []
+                    immune = []
+                    whisky = []
+                    ropes = []
+                    detect = []
+                    seek = []
+                    rumour = []
+                    
+                    people = []
+                    ids = []
+
+                    count = 0
+                    send_message(sender, "Да будет аукцион!")
+                    #for j in range(len(admins)):
+                    #    send_message(admins[j], "Да будет аукцион!")
+                    #    j += 1
+                    break
+                elif sender != admins[i]:
+                    i += 1
+        elif rm == "хочу контракт":
+            send_button(sender, "Ciao!\nМеня зовут Роберто, и я тут что-то вроде старшего вожатого. Слежу за порядком, продумываю план смены, заведую всякими бумажными делами... Кстати о бумажных делах! Мне тут нужно каждому новенькому выдать по подарочку. Единственное: для этого нужна одна очень интересная вещица, мы с тобой поменяемся, её можно взять у... О, я вижу, вы уже знакомы с моей дорогой невестой Стеллой! Тогда давай сюда контракт на оказание услуг.\nAttendi, вношу тебя в ведомость... Готово! Теперь можешь выбрать что-нибудь из списка ниже. Только не расстраивайся, если кто-то успел забрать это раньше тебя! Всё-таки в лагере имеет место определённый соревновательный момент.")
+        elif rm == "список":
+            for i in range(len(admins)):
+                if sender == admins[i]:
+                    itog = finally_end()
+                    send_message(sender, itog)
+                else:
+                    i += 1
+        elif rm[:5] == "взять":
+            false = 0
+            k = 0
+            for k in range(len(ids)):
+                if sender == ids[k]:
+                    send_message(sender, "Mamma mia! А кто это тут пытается забрать себе второй контракт? Ты смотри, спрячь свои загребущие ручки подальше, всё должно быть по-честному! Маленький озорник!")
+                    false = 1
+                    break
+                else:
+                    k += 1
+            if false != 1:
+                rm = rm[6:]
+                if rm == "иммунитет":
+                    if immune0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        immune.append(sayer_name)
+                        ids.append(sender)
+                        attachments = immune3
+                        deal(sender, attachments, immune1)
+                        immune0 -= 1
+                        count += 1
+                elif rm == "слежку":
+                    if seek0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        seek.append(sayer_name)
+                        ids.append(sender)
+                        attachments = seek3
+                        deal(sender, attachments, seek1)
+                        seek0 -= 1
+                        count += 1
+                elif rm == "убийство":
+                    if kill0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        kill.append(sayer_name)
+                        ids.append(sender)
+                        attachments = kill3
+                        deal(sender, attachments, kill1)
+                        kill0 -= 1
+                        count += 1
+                elif rm == "исцеление":
+                    if heal0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        heal.append(sayer_name)
+                        ids.append(sender)
+                        attachments = heal3
+                        deal(sender, attachments, heal1)
+                        heal0 -= 1
+                        count += 1
+                elif rm == "бутылку виски":
+                    if whisky0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        whisky.append(sayer_name)
+                        ids.append(sender)
+                        attachments = whisky3
+                        deal(sender, attachments, whisky1)
+                        whisky0 -= 1
+                        count += 1
+                elif rm == "бондаж":
+                    if ropes0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        ropes.append(sayer_name)
+                        ids.append(sender)
+                        attachments = ropes3
+                        deal(sender, attachments, ropes1)
+                        ropes0 -= 1
+                        count += 1
+                elif rm == "частное расследование":
+                    if detect0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        detect.append(sayer_name)
+                        ids.append(sender)
+                        attachments = detect3
+                        deal(sender, attachments, detect1)
+                        detect0 -= 1
+                        count += 1
+                elif rm == "аферу":
+                    if rumour0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        rumour.append(sayer_name)
+                        ids.append(sender)
+                        attachments = rumour3
+                        deal(sender, attachments, rumour1)
+                        rumour0 -= 1
+                        count += 1
+                elif rm == "раскрытие роли":
+                    if find0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        find.append(sayer_name)
+                        ids.append(sender)
+                        attachments = find3
+                        deal(sender, attachments, find1)
+                        find0 -= 1
+                        count += 1
+                elif rm == "подмену роли":
+                    if close0 == 0:
+                        send_message(sender, "Mamma mia! Кажется, контракты этого типа уже закончились... Попробуй выбрать другой: вдруг тебе повезёт!")
+                    else:
+                        close.append(sayer_name)
+                        ids.append(sender)
+                        attachments = close3
+                        deal(sender, attachments, close1)
+                        close0 -= 1
+                        count += 1
+                else:
+                    send_message(sender, "Что-что?")
+                if count == 43:
+                    itog = finally_end()
+                    for i in range(len(admins)): 
+                        send_message(admins[i], itog)
+                        i += 1
+        else:
+            send_message(sender, "Не понимаю...")
